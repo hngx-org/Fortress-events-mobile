0f9089535d18747b188097b77c7508b830c2d0a0
Moyinoluwa Shabi <shabimoyinoluwa19@gmail.com>
2023-09-19 23:58:36 +0100

    add generated colors & text styles from ui design
    - these are the exact colors and text styles used in the design and as specified by our designer in the "style" page of the figma file

---
+++ lib/src/core/utils/theme/text_styles.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+/// Generated with the Very Good Flutter Styles Figma Plugin by Very Good Ventures
+/// https://verygood.ventures
+
+abstract class AppTextStyles {
+  // textXsRegular Text Style
+  static const TextStyle textXsRegular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400, // regular
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textXsMeduim Text Style
+  static const TextStyle textXsMeduim = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500, // medium
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textXsSemibold Text Style
+  static const TextStyle textXsSemibold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600, // semiBold
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textXsBold Text Style
+  static const TextStyle textXsBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w700, // bold
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textSmallRegular Text Style
+  static const TextStyle textSmallRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400, // regular
+    height: 1.43,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textSmallMedium Text Style
+  static const TextStyle textSmallMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500, // medium
+    height: 1.43,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textSmallSemibold Text Style
+  static const TextStyle textSmallSemibold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600, // semiBold
+    height: 1.43,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textSmallBold Text Style
+  static const TextStyle textSmallBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700, // bold
+    height: 1.43,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textMdRegular Text Style
+  static const TextStyle textMdRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400, // regular
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textMdMedium Text Style
+  static const TextStyle textMdMedium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500, // medium
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textMdSemibold Text Style
+  static const TextStyle textMdSemibold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600, // semiBold
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textMdBold Text Style
+  static const TextStyle textMdBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700, // bold
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textLgRegular Text Style
+  static const TextStyle textLgRegular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400, // regular
+    height: 1.56,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textLgMedium Text Style
+  static const TextStyle textLgMedium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500, // medium
+    height: 1.56,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textLgSemibold Text Style
+  static const TextStyle textLgSemibold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600, // semiBold
+    height: 1.56,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textLgBold Text Style
+  static const TextStyle textLgBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700, // bold
+    height: 1.56,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textXlRegular Text Style
+  static const TextStyle textXlRegular = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400, // regular
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textXlMedium Text Style
+  static const TextStyle textXlMedium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500, // medium
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textXlSemibold Text Style
+  static const TextStyle textXlSemibold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600, // semiBold
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+  // textXlBold Text Style
+  static const TextStyle textXlBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700, // bold
+    height: 1.5,
+    fontFamily: 'Plus Jakarta Sans',
+  );
+}
