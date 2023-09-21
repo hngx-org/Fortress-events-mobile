9fbacc5f5f27a095d1885d727d0ec0afd62645c2
Moyinoluwa Shabi <shabimoyinoluwa19@gmail.com>
2023-09-21 20:03:53 +0100

    add `MyPeopleScreen` [100% done]
    also includes:
    - custom stacked_circle_avatars in the general_widgets folder with documentation
    - added missing color in `AppColors`
    - edits to `CustomIconContainer` docs

--- lib/src/features/people_groups/widgets/people_group_card.dart
+++ lib/src/features/people_groups/widgets/people_group_card.dart
@@ -1,6 +1,8 @@
 import 'package:event_app/src/core/constants/dimensions.dart';
 import 'package:event_app/src/core/utils/theme/colors.dart';
 import 'package:event_app/src/core/utils/theme/text_styles.dart';
+import 'package:event_app/src/features/people_groups/data/avatar_images.dart';
+import 'package:event_app/src/general_widgets/stacked_circle_avatars.dart';
 import 'package:event_app/src/general_widgets/custom_icon_container.dart';
 import 'package:event_app/src/general_widgets/custom_image_view.dart';
 import 'package:event_app/src/general_widgets/spacing.dart';
@@ -45,6 +47,7 @@ class PeopleGroupCard extends StatelessWidget {
                 color: AppColors.gray1000,
                 height: 24,
                 width: 24,
+                onTap: () {},
               ),
             ],
           ),
@@ -57,16 +60,25 @@ class PeopleGroupCard extends StatelessWidget {
           ),
           const Spacing.smallHeight(),
           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              StackedCircleAvatars(
+                avatarImages: randomImages,
+                remainingAvatarCount: '34',
+                avatarExternalRadius: 16,
+                avatarRadius: 14,
+                avatarCountExternalRadius: 16,
+                avatarCountRadius: 14,
+              ),
               CustomIconContainer(
                 containerText: '14 Events',
                 containerTextStyle: AppTextStyles.textXsMeduim.copyWith(
                   color: AppColors.primary1000,
+                  height: 1.35,
                 ),
                 containerColor: AppColors.accentGreen100,
                 containerHPadding: Dimensions.smedium,
-                containerVPadding: Dimensions.xsmall,
+                containerVPadding: Dimensions.small,
                 iconSvgPath: ImageConstant.imgCalendar,
                 iconColor: AppColors.primary1000,
                 iconHeight: 16,
