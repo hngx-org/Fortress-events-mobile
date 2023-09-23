4eb3abe2766b8f5cbc17d3ce7574f8a27f58ccc0
Edoki Chukwuyem <edokichuks@gmail.com>
2023-09-23 16:36:59 +0100

    google sign in working

--- lib/src/features/start_up/pages/homepage_two.dart
+++ lib/src/features/start_up/pages/homepage_two.dart
@@ -1,22 +1,22 @@
-import 'package:event_app/src/core/utils/app_enums.dart';
 import 'package:event_app/src/core/utils/theme/text_styles.dart';
-import 'package:event_app/src/features/auth/notifiers/user_notifier.dart';
-import 'package:event_app/src/features/auth/notifiers/user_state.dart';
 import 'package:event_app/src/features/calendar/pages/dashboard.dart';
-import 'package:event_app/src/features/start_up/pages/homepage_three.dart';
-import 'package:event_app/src/general_widgets/custom_elevated_button.dart';
-import 'package:event_app/src/general_widgets/first_homescreen_button.dart';
 import 'package:event_app/src/general_widgets/spacing.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../../../core/constants/dimensions.dart';
-import '../../../general_widgets/custom_homepage_button.dart';
 import '../../../general_widgets/custom_image_view.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:developer';
 
-class HomepageTwo extends StatelessWidget {
+class HomepageTwo extends StatefulWidget {
   const HomepageTwo({super.key});
   static const routeName = '/homepage-two-screen';
+
+  @override
+  State<HomepageTwo> createState() => _HomepageTwoState();
+}
+
+class _HomepageTwoState extends State<HomepageTwo> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
 
   @override
   Widget build(BuildContext context) {
@@ -59,59 +59,43 @@ class HomepageTwo extends StatelessWidget {
                   const SizedBox(
                     height: 20,
                   ),
-                  Consumer(
-                    builder: (context, ref, child) {
-                      final notifier = ref.read(userNotifierProvider.notifier);
-                      final state = ref.watch(userNotifierProvider);
-                      _navigateToHome(ref, context);
-                
-
-                      return GestureDetector(
-                        onTap: () {
-                          notifier.googleSign();
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //       builder: (context) => const Dashboard(),
-                          //     ));
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: Dimensions.medium),
-                          padding: const EdgeInsets.all(15.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30.0),
-                            // Rounded edges
-                            color: Colors.white,
-                            // White background color
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.1),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
+                  GestureDetector(
+                    onTap: _handleSignIn,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: Dimensions.medium),
+                      padding: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        // Rounded edges
+                        color: Colors.white,
+                        // White background color
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.1),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              CustomImageView(
-                                svgPath: 'assets/images/google_icon.svg',
-                              ),
-                              const SizedBox(width: 5.0),
-                              const Text(
-                                'Google',
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomImageView(
+                            svgPath: 'assets/images/google_icon.svg',
                           ),
-                        ),
-                      );
-                    },
+                          const SizedBox(width: 5.0),
+                          const Text(
+                            'Google',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                   const Spacing.height(52)
                 ],
@@ -122,16 +106,23 @@ class HomepageTwo extends StatelessWidget {
       ),
     );
   }
-        void _navigateToHome(WidgetRef ref, BuildContext context) async {
-    ref.listen<UserState>(userNotifierProvider, (previous, next) {
-      if (next.loadState == LoadState.success) {
+  Future<void> _handleSignIn() async {
+    try {
+      final userData = await _googleSignIn.signIn();
+      log('UserData: ${userData}');
+      if (userData != null) {
         Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => const Dashboard(),
             ));
-        return;
+      } else {
+        Navigator.pop(context);
+        log('Could not signup');
       }
-    });
+    } catch (error) {
+      log('Error from google auth => ${error.toString()}');
+      print(error);
+    }
   }
 }
