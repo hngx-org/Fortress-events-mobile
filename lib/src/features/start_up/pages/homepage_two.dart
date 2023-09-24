cf36258f000d6a738f24da6e35dcea1e203c38dc
Edoki Chukwuyem <edokichuks@gmail.com>
2023-09-24 12:25:59 +0100

    minor changes

--- lib/src/features/start_up/pages/homepage_two.dart
+++ lib/src/features/start_up/pages/homepage_two.dart
@@ -1,8 +1,6 @@
 import 'package:event_app/src/core/services/base_constants/logger.dart';
-import 'package:event_app/src/core/services/network/api_services.dart';
 import 'package:event_app/src/core/utils/theme/text_styles.dart';
 import 'package:event_app/src/features/auth/model/profile_details/create_account.dart';
-import 'package:event_app/src/features/auth/model/profile_details/user.dart';
 import 'package:event_app/src/features/auth/notifiers/user_notifier.dart';
 import 'package:event_app/src/features/calendar/pages/dashboard.dart';
 import 'package:event_app/src/general_widgets/spacing.dart';
@@ -51,20 +49,10 @@ class _HomepageTwoState extends ConsumerState<HomepageTwo> {
                 ),
               ),
               GestureDetector(
-                onTap: () {
-                  // Navigator.pushNamed(context, TimeLineHomepageThree.routeName);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const Dashboard(),
-                      ));
-                },
+                onTap: _handleSignIn,
                 child: Container(
-                  margin: const EdgeInsets.only(
-                    left: Dimensions.medium,
-                    right: Dimensions.medium,
-                    bottom: Dimensions.large,
-                  ),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: Dimensions.medium),
                   padding: const EdgeInsets.all(15.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30.0),
@@ -80,25 +68,11 @@ class _HomepageTwoState extends ConsumerState<HomepageTwo> {
                       ),
                     ],
                   ),
-                  GestureDetector(
-                    onTap: _handleSignIn,
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: Dimensions.medium),
-                      padding: const EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        // Rounded edges
-                        color: Colors.white,
-                        // White background color
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.1),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: const Offset(0, 3),
-                          ),
-                        ],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomImageView(
+                        svgPath: 'assets/images/google_icon.svg',
                       ),
                       const SizedBox(width: 5.0),
                       const Text(
@@ -112,6 +86,7 @@ class _HomepageTwoState extends ConsumerState<HomepageTwo> {
                   ),
                 ),
               ),
+              const Spacing.mediumHeight(),
             ],
           ),
         ),
@@ -128,7 +103,8 @@ class _HomepageTwoState extends ConsumerState<HomepageTwo> {
       if (userData != null) {
         final user = CreateAccount(
             photoUrl: userData.photoUrl == null
-                ? userData.photoUrl ?? 'https://th.bing.com/th/id/R.cec69a569d2d2224a738afb5ad8a419f?rik=VFoqor%2bvcZqRHA&riu=http%3a%2f%2fronaldmottram.co.nz%2fwp-content%2fuploads%2f2019%2f01%2fdefault-user-icon-8.jpg&ehk=KdwKcNMijBB2LcdEzKdT%2bIvtpFWIKQT2HetK7wCogdA%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1'
+                ? userData.photoUrl ??
+                    'https://th.bing.com/th/id/R.cec69a569d2d2224a738afb5ad8a419f?rik=VFoqor%2bvcZqRHA&riu=http%3a%2f%2fronaldmottram.co.nz%2fwp-content%2fuploads%2f2019%2f01%2fdefault-user-icon-8.jpg&ehk=KdwKcNMijBB2LcdEzKdT%2bIvtpFWIKQT2HetK7wCogdA%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1'
                 : 'https://th.bing.com/th/id/R.cec69a569d2d2224a738afb5ad8a419f?rik=VFoqor%2bvcZqRHA&riu=http%3a%2f%2fronaldmottram.co.nz%2fwp-content%2fuploads%2f2019%2f01%2fdefault-user-icon-8.jpg&ehk=KdwKcNMijBB2LcdEzKdT%2bIvtpFWIKQT2HetK7wCogdA%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
             email: userData.email,
             displayName: userData.displayName ?? '');
